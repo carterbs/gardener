a8d38b10d53d466998d01e4ed834e180987af1a0
Brad Carter <16466430+carterbs@users.noreply.github.com>
2026-03-01 16:39:13 -0500

    feat: implement task changes (#39)
    * feat: implement task changes
    
    * fix: merge remediation
    
    * feat: Enable clippy::redundant_static_lifetimes
    
    * feat: Enable clippy::redundant_static_lifetimes
    
    * fix: merge main into branch
    
    ---------
    
    Co-authored-by: Gardener Integration <integration@gardener.test>

--- scripts/check-no-warnings.sh
+++ scripts/check-no-warnings.sh
@@ -5,5 +5,5 @@ if [ "${BASH_SOURCE[0]}" = "$0" ]; then
   cd "$(git rev-parse --show-toplevel)"
 fi
 
-echo "Running no-warning clippy check: cargo clippy -p gardener --all-targets -- -D warnings -A clippy::expect_used"
-cargo clippy -p gardener --all-targets -- -D warnings -A clippy::expect_used
+echo "Running no-warning clippy check: cargo clippy -p gardener --all-targets -- -D warnings -A clippy::expect_used -W clippy::redundant_static_lifetimes"
+cargo clippy -p gardener --all-targets -- -D warnings -A clippy::expect_used -W clippy::redundant_static_lifetimes

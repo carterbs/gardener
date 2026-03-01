e49bddb69c105eec8a6d2d5f62287f4997062084
Gardener Integration <integration@gardener.test>
2026-03-01 15:08:34 -0500

    feat: Enable clippy::unnecessary_to_owned

--- scripts/check-no-warnings.sh
+++ scripts/check-no-warnings.sh
@@ -5,5 +5,5 @@ if [ "${BASH_SOURCE[0]}" = "$0" ]; then
   cd "$(git rev-parse --show-toplevel)"
 fi
 
-echo "Running no-warning clippy check: cargo clippy -p gardener --all-targets -- -D warnings -A clippy::expect_used -W clippy::redundant_static_lifetimes"
-cargo clippy -p gardener --all-targets -- -D warnings -A clippy::expect_used -W clippy::redundant_static_lifetimes
+echo "Running no-warning clippy check: cargo clippy -p gardener --all-targets -- -D warnings -A clippy::expect_used -W clippy::redundant_static_lifetimes -W clippy::unnecessary_to_owned"
+cargo clippy -p gardener --all-targets -- -D warnings -A clippy::expect_used -W clippy::redundant_static_lifetimes -W clippy::unnecessary_to_owned
